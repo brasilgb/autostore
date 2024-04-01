b7db16cc0a99128e97533add73f87dc183d3f97b
Anderson Rodrigues <anderson.rodrigues@gruposolar.com.br>
2024-04-01 17:37:01 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,30 @@
+const axios = require("axios");
+const csv = require("csvtojson");
+
+function csvtojson(url, type) {
+
+    const csvFilePath = `${url}` //file path of csv
+    csv({
+        delimiter: '|'
+    })
+        .fromFile(csvFilePath)
+        .then(async (jsonObj) => {
+            try {
+                const response = await axios.post('http://localhost:8000/api/datainsert',
+                    {
+                        Headers: {
+                            "Content-Type": "application/json"
+                        },
+                            type: type,
+                            dbdata: jsonObj
+                    }
+                );
+                const verostore = response.data.response.message;
+                console.log(verostore);
+            } catch (error) {
+                console.log(error);
+            }
+        })
+}
+csvtojson(url = "C:/Users/anderson.rodrigues/Documents/filesftp/sale/852917526-sale.csv", "sale");
+csvtojson(url = "C:/Users/anderson.rodrigues/Documents/filesftp/user/852917526-user.csv", "user");

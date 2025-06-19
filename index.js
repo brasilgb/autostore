d4b6b247c30153d97e61f9710b045172294c880f
AndersonBrasil <andersonbrasil72@gmail.com>
2025-06-19 16:10:33 -0300

    push

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ function csvtojson(url, type) {
         .then(async (jsonObj) => {
             try {
                 // const response = await axios.post('https://automagic.megb.com.br/api/datainsert',
-                const response = await axios.post('http://localhost:3000/api/pushdata',
+                const response = await axios.post('http://localhost:8000/api/uploaddata',
                     {
                         Headers: {
                             "Content-Type": "application/json"
